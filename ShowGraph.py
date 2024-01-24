59c1e35ccd8d78efd0b5b2e812f17e707521a0ad
Yida Zhang <zyd200101@outlook.com>
2024-01-24 16:47:45 -0500

    xdot

--- ShowGraph.py
+++ ShowGraph.py
@@ -6,4 +6,4 @@ def view(G, prefix = ''):
     fd, dot = tempfile.mkstemp(prefix=prefix, suffix='.dot')
     os.close(fd)
     nx.nx_agraph.write_dot(G, dot)
-    subprocess.Popen(['/usr/bin/xdg-open', '{}'.format(dot)])
+    subprocess.Popen(['/usr/bin/xdot', '{}'.format(dot)])
